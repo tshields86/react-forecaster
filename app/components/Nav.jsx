0fc2b0c9b2cd63691d9a5b1df087db8f6feb9686
Travis Shields <travis.shields@gmail.com>
2016-07-23 15:30:32 -0400

    Set up react router

---
+++ app/components/Nav.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Nav = React.createClass({
+  render: function () {
+    return (
+      <h3>Navigation component</h3>
+    );
+  }
+});
+
+module.exports = Nav;
