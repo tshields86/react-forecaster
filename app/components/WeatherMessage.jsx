7165140010ba046be7d2c485fbc3b0fafedcb175
Travis Shields <travis.shields@gmail.com>
2016-07-23 20:04:22 -0400

    Destructured stateless components

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,14 +1,11 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-  render: function () {
-    var {temp, location} = this.props;
-    return (
-      <div>
-        <h3>It is {temp} in {location}.</h3>
-      </div>
-    );
-  }
-});
+var WeatherMessage = ({temp, location}) => {
+  return (
+    <div>
+      <h3>It is {temp} in {location}.</h3>
+    </div>
+  )
+};
 
 module.exports = WeatherMessage;
--- webpack.config.js
+++ webpack.config.js
@@ -30,5 +30,6 @@ module.exports = {
         exclude: /(node_modules|bower_components)/
       }
     ]
-  }
+  },
+  devtool: 'cheap-module-eval-source-map'
 };
